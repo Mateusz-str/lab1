40aae508ea41417e6767b228a29c26bbd770ab47
Mateusz-str <226149@student.pwr.edu.pl>
2016-03-22 22:21:00 +0100

    1

--- main.cpp
+++ main.cpp
@@ -7,17 +7,30 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+	srand(atoi(argv[2])) ;
+	int n = atoi(argv[1]);
+	drawCharSquare(charSquare(n), n);
+	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square = new char *[n];
+	for(int i=0;i<n*n;i++)
+		square[i]= new char[n];
+	for(int i=0;i<n*n;i++)
+		square[i/n][i%n] = rand()%26+97;
+
 	return square;
+	
 }
 
 void drawCharSquare(char **square, int n)
 {
+	for (int i = 0; i < n*n; i++){
+ 		(i%n)==(n-1)?printf("%-2c\n", square[i/n][i%n]):printf("%-2c", square[i/n][i%n]);
+ 	}
+	
 
+		
 }
